f45ff061353dedbf6a2c0e791affe2ef71fac28c
Umidjon Khodjaev <iexistedonlyforyou@gmail.com>
2026-02-23 23:37:37 +0500

    nothing

--- src/models/category/constants.py
+++ src/models/category/constants.py
@@ -4,7 +4,7 @@ from typing import Annotated, Optional
 
 # Validation Constants
 SKU_VALID = Annotated[
-    str, Field(pattern=r'^[A-Z0-9]{3,20}$', min_length=1, max_length=50, description='Unique SKU (Stock Keeping Unit)')
+    str, Field(pattern=r'^[A-Z0-9]{3,20}$', min_length=1, max_length=50, description='Unique SKU (Category code)')
 ]
 NAME_VALID = Annotated[str, Field(min_length=1, max_length=50, json_schema_extra={'strip_whitespace': True})]
 DES_VALID = Annotated[Optional[str], Field(max_length=300)]
