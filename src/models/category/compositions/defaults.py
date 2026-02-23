8a53a3536213220abea95d6544ccc1693c6d008d
Umidjon Khodjaev <iexistedonlyforyou@gmail.com>
2026-02-23 23:50:37 +0500

    changed compostion default logic to properly work

--- src/models/category/compositions/defaults.py
+++ src/models/category/compositions/defaults.py
@@ -39,7 +39,7 @@ class CtgDefaults(BaseModel):
     )
     default_packaging_type: Annotated[Optional[PackagingType], Field(description='Packaging type by default')] = None
     default_shelf_life_days: Annotated[Optional[POSITIVE_INT], Field(description='Default expiry date')] = None
-    default_unit_of_measure: Annotated[Optional[UnitOfMeasure], Field(description='Deafult unit of measure')] = None
+    default_unit_of_measure: Annotated[Optional[UnitOfMeasure], Field(description='Deafault unit of measure')] = None
 
     # ----------- Validators --------
     # ------- Hazard class Consistensy ------
@@ -59,16 +59,17 @@ class CtgDefaults(BaseModel):
     @model_validator(mode='after')
     def check_temperature_required(self) -> 'CtgDefaults':
         """If storage_condition requires temperature control, temperature_regime must be provided."""
-        if (
-            self.default_storage_condition
-            in (
-                ProductStorageCondition.PERISHABLE,
-                ProductStorageCondition.TEMPERATURE_CONTROLLED,
-                ProductStorageCondition.MEDICINE,
-            )
-            and self.default_temperature_regime is None
-        ):
-            raise ValueError('temperature_regime required for perishable or temperature-controlled products')
+        if self.default_storage_condition and self.default_temperature_regime:
+            if (
+                self.default_storage_condition
+                in (
+                    ProductStorageCondition.PERISHABLE,
+                    ProductStorageCondition.TEMPERATURE_CONTROLLED,
+                    ProductStorageCondition.MEDICINE,
+                )
+                and self.default_temperature_regime is None
+            ):
+                raise ValueError('temperature_regime required for perishable or temperature-controlled products')
         return self
 
     # --------- Recommendations ----------
@@ -83,35 +84,50 @@ class CtgDefaults(BaseModel):
             print('Recommendation: For product type Electronics recommendts temperature_regime')
         return self
 
+    # ------- Storag Requirements Condition -------
+    @model_validator(mode='after')
+    def validate_stgc_requirements(self) -> 'CtgDefaults':
+        """Validating Product Storage Condition types requirements"""
+        # Validating for Perishable and Traceability products requirements
+        if self.default_storage_condition and self.default_tracking_type:
+            if (
+                self.default_storage_condition in (ProductStorageCondition.PERISHABLE, ProductStorageCondition.MEDICINE)
+                and self.default_tracking_type != ProductTrackingType.EXPIRY_TRACKED
+            ):
+                raise ValueError('Perishable and Medicine products must have tracking_type = EXPIRY_TRACKED')
+        return self
+
     # -------- Tracking Type -------
     @model_validator(mode='after')
     def validate_tracking_type_units(self) -> 'CtgDefaults':
         """Validating units of measure for Product Tracking type units"""
-        # Weight Based
-        if self.default_tracking_type == ProductTrackingType.WEIGHT_BASED:
-            allowed = {
-                UnitOfMeasure.KILOGRAM,
-                UnitOfMeasure.GRAM,
-                UnitOfMeasure.LITER,
-                UnitOfMeasure.MILLILITER,
-                UnitOfMeasure.CUBIC_METER,
-            }
-            if self.default_unit_of_measure not in allowed:
-                raise ValueError(
-                    f'Weight-based tracking requires unit_of_measure in {allowed}, got {self.default_unit_of_measure}'
-                )
-        # Piece
-        elif self.default_tracking_type == ProductTrackingType.PIECE:
-            allowed = {UnitOfMeasure.PIECE, UnitOfMeasure.BOX, UnitOfMeasure.PALLET, UnitOfMeasure.SET}
-            if self.default_unit_of_measure not in allowed:
-                raise ValueError(
-                    f'Piece tracking requires unit_of_measure in {allowed}, got {self.default_unit_of_measure}'
-                )
-        # Kit
-        elif self.default_tracking_type == ProductTrackingType.KIT:
-            allowed = {UnitOfMeasure.SET, UnitOfMeasure.PIECE}
-            if self.default_unit_of_measure not in allowed:
-                raise ValueError(
-                    f'Kit tracking requires unit_of_measure in {allowed}, got {self.default_unit_of_measure}'
-                )
+        if self.default_tracking_type and self.default_unit_of_measure:
+            # Weight Based
+            if self.default_tracking_type == ProductTrackingType.WEIGHT_BASED:
+                allowed = {
+                    UnitOfMeasure.KILOGRAM,
+                    UnitOfMeasure.GRAM,
+                    UnitOfMeasure.LITER,
+                    UnitOfMeasure.MILLILITER,
+                    UnitOfMeasure.CUBIC_METER,
+                }
+                if self.default_unit_of_measure not in allowed:
+                    raise ValueError(
+                        'Weight-based tracking requires unit_of_measure'
+                        f' in {allowed}, got {self.default_unit_of_measure}'
+                    )
+            # Piece
+            elif self.default_tracking_type == ProductTrackingType.PIECE:
+                allowed = {UnitOfMeasure.PIECE, UnitOfMeasure.BOX, UnitOfMeasure.PALLET, UnitOfMeasure.SET}
+                if self.default_unit_of_measure not in allowed:
+                    raise ValueError(
+                        f'Piece tracking requires unit_of_measure in {allowed}, got {self.default_unit_of_measure}'
+                    )
+            # Kit
+            elif self.default_tracking_type == ProductTrackingType.KIT:
+                allowed = {UnitOfMeasure.SET, UnitOfMeasure.PIECE}
+                if self.default_unit_of_measure not in allowed:
+                    raise ValueError(
+                        f'Kit tracking requires unit_of_measure in {allowed}, got {self.default_unit_of_measure}'
+                    )
         return self
